20c596ac343af1f4d9376c2109195e976e9661c1
Zhankui Wang <tlisa2053@gmail.com>
2024-09-25 10:58:02 -0400

    add chat history and session

--- src/components/ChatWindow.vue
+++ src/components/ChatWindow.vue
@@ -14,7 +14,12 @@
         <!-- <div v-for="(message, index) in userMessages" :key="index" class="message" :class="message.type">
           {{ message.text }}
         </div> -->
-        <div v-for="(message, index) in userMessages" :key="index" class="message" :class="message.type">
+        <div
+          v-for="(message, index) in userMessages"
+          :key="index"
+          class="message"
+          :class="message.type"
+        >
           {{ message.text }}
         </div>
       </div>
@@ -24,11 +29,16 @@
     <a-layout-footer>
       <a-row :gutter="16">
         <!-- Input field occupying most of the row -->
-        <a-col :xs="18" sm="19" md="22" lg="22" xl="23" >
-          <a-input :value="userInput" @input="handleInput" placeholder="请输入您要咨询的内容" @keypress.enter="submitMessage" />
+        <a-col :xs="18" sm="19" md="22" lg="22" xl="23">
+          <a-input
+            :value="userInput"
+            @input="handleInput"
+            placeholder="请输入您要咨询的内容"
+            @keypress.enter="submitMessage"
+          />
         </a-col>
         <!-- Button occupying a small part of the row -->
-        <a-col :xs="6" sm="5" md="2" lg="2" xl="1" >
+        <a-col :xs="6" sm="5" md="2" lg="2" xl="1">
           <a-button type="primary" block @click="submitMessage">提交</a-button>
         </a-col>
       </a-row>
@@ -37,39 +47,40 @@
 </template>
 
 <script>
-import { Input, Button, Layout, Row, Col } from 'ant-design-vue';
-import { ref, onMounted, onBeforeUnmount } from 'vue';
+import { Input, Button, Layout, Row, Col } from "ant-design-vue";
+import { ref, onMounted, onBeforeUnmount } from "vue";
 
 export default {
-  name: 'chatwindow',
+  name: "chatwindow",
   components: {
-    'a-input': Input,
-    'a-button': Button,
-    'a-layout': Layout,
-    'a-layout-content': Layout.Content,
-    'a-layout-footer': Layout.Footer,
-    'a-row': Row,
-    'a-col': Col,
+    "a-input": Input,
+    "a-button": Button,
+    "a-layout": Layout,
+    "a-layout-content": Layout.Content,
+    "a-layout-footer": Layout.Footer,
+    "a-row": Row,
+    "a-col": Col,
   },
   setup() {
-    const userInput = ref('');
+    const userInput = ref("");
     const userMessages = ref([]); // Store messages as an array
     let socket = null;
     // let currentBotMessage = ''; // Temporary variable to hold bot message
     let currentBotMessageIndex = null; // Index of the current bot message being updated
-
+    let sessionId = sessionStorage.getItem("session_id");
+    const API_URL = import.meta.env.VITE_API_URL;
 
     const submitMessage = () => {
       if (userInput.value.trim()) {
         // console.log(userInput.value)
         // Push user message to the messages array
-        userMessages.value.push({ text: userInput.value, type: 'user' });
+        userMessages.value.push({ text: userInput.value, type: "user" });
         if (socket) {
           socket.send(JSON.stringify({ question: userInput.value }));
         }
-        userInput.value = ''; // Clear the input after submission
+        userInput.value = ""; // Clear the input after submission
         // currentBotMessage='';
-        currentBotMessageIndex=0;
+        currentBotMessageIndex = 0;
       }
     };
 
@@ -79,41 +90,70 @@ export default {
 
     const handleIncomingMessage = (event) => {
       const data = JSON.parse(event.data);
-      if (data.message) {
+
+      // if the response data don't type property but have message property, means it's robot generated
+      if (data.message && !data.type) {
         if (currentBotMessageIndex === 0) {
           // Push a new bot message placeholder when receiving the first chunk
-          userMessages.value.push({ text: '', type: 'bot' });
+          userMessages.value.push({ text: "", type: "bot" });
           // console.log(userMessages.value.length)
           currentBotMessageIndex = userMessages.value.length - 1; // Save the index of the current bot message
         }
         // Concatenate the incoming chunk to the current bot message
         userMessages.value[currentBotMessageIndex].text += data.message;
       }
+      // if the response data have messages property, means it's chat history from redis
+      else if (data.messages) {
+        // Update chat with recent messages
+        userMessages.value = data.messages || [];
+      }
+      // if the response data have session_id property, means it's first create the session id
+      else if (data.session_id) {
+        sessionId = data.session_id;
+        sessionStorage.setItem("session_id", sessionId);
+      }
     };
 
-    onMounted(() => {
-      const sessionId = document.cookie.split('; ')
-    .find(row => row.startsWith('sessionid'))
-    ?.split('=')[1];
-      // Establish WebSocket connection
-      socket = new WebSocket(`ws://localhost:8000/ws/chat/?sessionid=${sessionId}`);
+    // const  handleBeforeUnload= async () =>{
+    //   const sessionId = sessionStorage.getItem("session_id");
+    //   if (sessionId) {
+    //     try {
+    //       await axios.post(`${API_URL}api/v1/chat/delete_session/`, {
+    //         session_id: sessionId,
+    //       });
+    //       sessionStorage.removeItem("session_id"); // Optionally remove session ID from sessionStorage
+    //     } catch (error) {
+    //       console.error("Error deleting session:", error);
+    //     }
+    //   }
+    // };
 
+    onMounted(() => {
+      const finalSessionId =
+        sessionId && sessionId !== "undefined" ? sessionId : "";
+      // Establish WebSocket connection
+      socket = new WebSocket(
+        `ws://localhost:8000/ws/chat/?sessionid=${finalSessionId}`
+      );
+
+      // window.addEventListener("beforeunload", handleBeforeUnload);
       socket.onmessage = handleIncomingMessage; // Set the message handler
 
       socket.onopen = () => {
-        console.log('WebSocket connection established');
+        console.log("WebSocket connection established");
       };
 
       socket.onclose = () => {
-        console.log('WebSocket connection closed');
+        console.log("WebSocket connection closed");
       };
 
       socket.onerror = (error) => {
-        console.error('WebSocket error:', error);
+        console.error("WebSocket error:", error);
       };
     });
 
     onBeforeUnmount(() => {
+      // window.removeEventListener("beforeunload", handleBeforeUnload);
       if (socket) {
         socket.close(); // Clean up the WebSocket connection
       }
@@ -123,12 +163,11 @@ export default {
       userInput,
       userMessages,
       submitMessage,
-      handleInput
+      handleInput,
     };
   },
 };
 </script>
-
 
 <style scoped>
 .customer-service {
